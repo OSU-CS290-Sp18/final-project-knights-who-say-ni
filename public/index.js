10057278972e87658d3c4a447fd8eb04df6a2d9d
Bienfu <bairdd@oregonstate.edu>
2018-06-11 00:22:48 -0700

    fixed json file and started on index.js which is needed to generate random burgers via the math.random function. Also needed to complete the handlebar creation

---
+++ public/index.js
@@ -0,0 +1,24 @@
+function handleGenerateClick(){
+    var rand1 = getRandom();
+    var rand2 = getRandom();
+    var rand3 = getRandom();
+    var rand4 = getRandom();
+    var rand5 = getRandom();
+
+}
+
+function getRandom() {
+    return (Math.random()%4);
+}
+
+window.addEventListener('DOMContent"Loaded', function() {
+
+    var generateButton =  document.getElementById('generateButton');
+    generateButton = addEventListener('click', handleGenerateClick);
+
+    var yuckButton = document.getElementsByClassName('yuck');
+    yuckButton = addEventListener('click', handleGenerateClick);
+
+
+
+});
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
+
+var ingredients = require('./ingredientList');
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -9,9 +12,23 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
-app.get('/', function (req, res, next){
+app.get('/home', function (req, res, next){
    res.status(200).render('home');
 });
+
+app.get('/generate', function (req, res, next){
+    res.status(200).render('generate',{
+        ingredients: ingredientList
+    });
+ });
+
+ app.get('/liability', function (req, res, next){
+    res.status(200).render('liability');
+ });
+
+ app.get('/order', function (req, res, next){
+    res.status(200).render('order');
+ });
 
 app.get('*', function (req, res, next){
    res.status(400).render('404');
