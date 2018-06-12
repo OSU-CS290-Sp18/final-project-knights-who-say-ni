c1e21e596fcb82a57ff3cc00fa19bdf0fcbc0d74
rise3n <38195409+rise3n@users.noreply.github.com>
2018-06-12 16:12:18 -0700

    Update server.js
    generate random ingredients from the database and use it to render website

--- server.js
+++ server.js
@@ -6,19 +6,35 @@ var ingredientList = require('./ingredientList');
 
 var app = express();
 var port = process.env.PORT || 3000;
-
+app.use(express.static('public'));
 app.engine('handlebars', exphbs({defaultLayout: 'layout'}));
 app.set('view engine', 'handlebars');
-
-app.use(express.static('public'));
 
 app.get('/home', function (req, res, next){
    res.status(200).render('home');
 });
 
 app.get('/generate', function (req, res, next){
-    res.status(200).render('generate',{
-        ingredients: ingredientList
+    var rand1 = Math.floor(Math.random() * 3 + 1);
+    var rand2 = Math.floor(Math.random() * 3 + 1);
+    var rand3 = Math.floor(Math.random() * 3 + 1);
+    var rand4 = Math.floor(Math.random() * 3 + 1);
+    var rand5 = Math.floor(Math.random() * 3 + 1);
+    var bun = ingredientList.bun[rand1];
+    var drink = ingredientList.drink[rand2];
+    var meat = ingredientList.meat[rand3];
+    var side = ingredientList.side[rand4];
+    var veggie = ingredientList.veggies[rand5];
+    var randingredient = {
+        bun:bun,
+        drink:drink,
+        meat:meat,
+        side: side,
+        veggie:veggie
+    };
+   
+    res.status(200).render('generate', {
+        ingredients: randingredient
     });
  });
 
