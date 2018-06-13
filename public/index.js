88ea08504e9d1b3ead7f5bc2f0d9054921994f73
Kyle Peterson <kylepeterson17@gmail.com>
2018-06-12 19:20:46 -0700

    added mongo support and cleaned up some links and js

--- public/index.js
+++ public/index.js
@@ -1,10 +1,5 @@
 function handleGenerateClick(){
-    var rand1 = getRandom();
-    var rand2 = getRandom();
-    var rand3 = getRandom();
-    var rand4 = getRandom();
-    var rand5 = getRandom();
-
+	window.location.href = '/generate';
 }
 
 function getRandom() {
@@ -16,9 +11,25 @@ window.addEventListener('DOMContent"Loaded', function() {
     var generateButton =  document.getElementById('generateButton');
     generateButton = addEventListener('click', handleGenerateClick);
 
-    var yuckButton = document.getElementsByClassName('yuck');
+    var yuckButton = document.querySelector('.yuck');
     yuckButton = addEventListener('click', handleGenerateClick);
 
 
 
 });
+
+function changeActive() {
+	var urlPath = window.location.pathname.toLowerCase();
+	var navItems = document.querySelectorAll('.navitem');
+
+	navItems.forEach(function(item) {
+		item.classList.remove('active');
+		if(item.childNodes[0].getAttribute("href").toLowerCase() === urlPath){
+			item.classList.add('active');
+		};
+	});
+}
+
+window.onload = function() {
+	changeActive();
+}
--- server.js
+++ server.js
@@ -1,41 +1,62 @@
 var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
+var MongoClient = require('mongodb').MongoClient;
 
 var ingredientList = require('./ingredientList');
 
 var app = express();
-var port = process.env.PORT || 3000;
-app.use(express.static('public'));
+var port = process.env.PORT || 17177;
+
+var mongoHost = process.env.MONGO_HOST || 'classmongo.engr.oregonstate.edu';
+var mongoPort = process.env.MONGO_PORT || '27017';
+var mongoUsername = process.env.MONGO_USERNAME || 'cs290_peterkyl';
+var mongoPassword = process.env.MONGO_PASSWORD || 'cs290_peterkyl';
+var mongoDBName = process.env.MONGO_DB_NAME || 'cs290_peterkyl';
+var mongoURL = "mongodb://" +
+  mongoUsername + ":" + mongoPassword + "@" + mongoHost + ":" + mongoPort +
+    "/" + mongoDBName;
+var mongoDB = null;
+
 app.engine('handlebars', exphbs({defaultLayout: 'layout'}));
 app.set('view engine', 'handlebars');
 
-app.get('/home', function (req, res, next){
+app.get(['/home','/'], function (req, res, next){
    res.status(200).render('home');
 });
 
 app.get('/generate', function (req, res, next){
-    var rand1 = Math.floor(Math.random() * 3 + 1);
-    var rand2 = Math.floor(Math.random() * 3 + 1);
-    var rand3 = Math.floor(Math.random() * 3 + 1);
-    var rand4 = Math.floor(Math.random() * 3 + 1);
-    var rand5 = Math.floor(Math.random() * 3 + 1);
-    var bun = ingredientList.bun[rand1];
-    var drink = ingredientList.drink[rand2];
-    var meat = ingredientList.meat[rand3];
-    var side = ingredientList.side[rand4];
-    var veggie = ingredientList.veggies[rand5];
-    var randingredient = {
+	 var ingredientCollection = mongoDB.collection('ingredients');
+	 var ingredient;
+	 ingredientCollection.find().toArray(function (err, ingredientArr){
+		 if(err){
+		 	console.log("error! using backup json");
+		 	ingredient = ingredientList;
+		 } else {
+		   ingredient = ingredientArr[0];
+		 };
+ 	  	 var rand1 = Math.floor(Math.random() * 3 + 1);
+    	 var rand2 = Math.floor(Math.random() * 3 + 1);
+	    var rand3 = Math.floor(Math.random() * 3 + 1);
+   	 var rand4 = Math.floor(Math.random() * 3 + 1);
+	    var rand5 = Math.floor(Math.random() * 3 + 1);
+   	 var bun = ingredient.bun[rand1];
+	    var drink = ingredient.drink[rand2];
+	    var meat = ingredient.meat[rand3];
+   	 var side = ingredient.side[rand4];
+	    var veggie = ingredient.veggies[rand5];
+	    var randingredient = {
         bun:bun,
         drink:drink,
         meat:meat,
         side: side,
         veggie:veggie
-    };
-   
-    res.status(200).render('generate', {
-        ingredients: randingredient
+    	 };
+   	 //console.log(randingredient);
+    	 res.status(200).render('generate', {
+       	ingredients: randingredient
     });
+	});
  });
 
  app.get('/liability', function (req, res, next){
@@ -46,10 +67,19 @@ app.get('/generate', function (req, res, next){
     res.status(200).render('order');
  });
 
+app.use('/images',express.static('images'));
+app.use(express.static('public'));
+
 app.get('*', function (req, res, next){
    res.status(400).render('404');
 });
 
-app.listen(port, function(){
-   console.log("== Server is listenting on port",port);
-});
+MongoClient.connect(mongoURL, function (err, client) {
+	if (err) {
+		throw err;
+	}
+	mongoDB = client.db(mongoDBName);
+	app.listen(port, function () {
+		console.log("== Server listening on port", port);
+	});
+})
